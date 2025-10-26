0ee6da7ce4efaf016c8426f508fde75909d3cfe6
rcs64 <rcs64@gcloud.ua.es>
2025-10-26 19:04:47 +0100

    Login acabado

--- js/login.js
+++ js/login.js
@@ -32,6 +32,66 @@ function misubmit( evt ) {
 
     if(emailValido && passValida)
         document.getElementById("formularioLogin").submit();
+    else
+        ventanaModal(!emailValido, !passValida);
+}
+
+function ventanaModal( email, pass ) {
+    let modal = document.createElement('section');
+    modal.style.position = 'fixed';
+    modal.style.top = '0';
+    modal.style.left = '0';
+    modal.style.width = '100%';
+    modal.style.height = '100%';
+    modal.style.backgroundColor = 'rgba(255, 255, 255, 0.18)';
+    modal.style.display = 'flex';        
+    modal.style.justifyContent = 'center';
+    modal.style.alignItems = 'center';
+    modal.style.zIndex = '1000';
+
+    let modalContent = document.createElement('section');
+    modalContent.style.backgroundColor = 'rgba(255, 255, 255, 0.8)';
+    modalContent.style.padding = '20px';
+    modalContent.style.borderRadius = '5px';
+    modalContent.style.textAlign = 'center';
+
+    let message = document.createElement('p');
+    
+    let cosas_mal = [];
+
+    if(email)
+        cosas_mal.push('email');
+
+    if(pass)
+        cosas_mal.push('contraseña');
+
+    let texto_mensaje = `   No se puede dejar ningún campo en blanco. \n 
+                        Esto incluye poner solo espacios o solo tabulaciones.
+                        Campos erróneos: `
+    
+    for(let i=0; i<cosas_mal.length; i++) {
+        if(i != cosas_mal.length-1)
+            texto_mensaje += cosas_mal[i]+', ';
+        else
+            texto_mensaje += cosas_mal[i];
+    }
+
+    message.textContent = texto_mensaje;
+
+
+
+    let closeButton = document.createElement('button');
+    closeButton.textContent = 'Cerrar';
+    closeButton.style.marginTop = '10px';
+    closeButton.onclick = function () {
+
+        document.body.removeChild(modal); // Eliminar la ventana modal
+    };
+    
+    modalContent.appendChild(message);
+    modalContent.appendChild(closeButton);
+    modal.appendChild(modalContent);
+    document.body.appendChild(modal); // Agregar la ventana modal al documento 
 }
 
 function restaurarEstilo (id) {

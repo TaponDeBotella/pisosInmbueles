eed3f0532878d011194fca4edce4944194aca9c0
Ari <ariguillenua@gmail.com>
2025-11-09 22:40:39 +0100

    lo de enviar mensaje hecho

--- enviar_mensaje.php
+++ enviar_mensaje.php
@@ -5,26 +5,36 @@
     $title="Enviar mensaje";
     $acceder = "Acceder";
     $css="css/enviar_mensaje.css";
-    include 'includes/header.php'; 
+    $css_errores = "css/errores.css";
+    include 'includes/header.php';
+    
+    $texto_error = false;
+    
+    // primero se comprueba si en la url hay un error
+    if(isset($_GET['error'])) {
+        if($_GET['error'] == 'texto_vacio') {
+            $texto_error = true;
+        }
+    }
 ?>
 
         <h1>Enviar mensaje</h1>
-        <form>
+        <form action="respuesta_mensaje.php" method="POST">
             <label id="texto_mensaje">Texto del mensaje</label>
-            <textarea>
+            <textarea name="texto_mensaje">
                 
             </textarea>
+            <?php 
+                if($texto_error) // si hay un error entonces se le muestra al usuario el mensaje y no se le permite enviar el texto vacio
+                    echo '<span class="mensaje-error">El mensaje no puede estar vacío</span>'; 
+            ?>
             <label id="tipo_mensaje">Tipo de mensaje</label>
-            <select class="boton" id="msg_type">
+            <select class="boton" id="msg_type" name="msg_type">
                 <option value="mas_info">Más información</option>
                 <option value="cita">Solicitar una cita</option>
                 <option value="oferta">Comunicar una oferta</option>
             </select>
-            <input class="boton" type="submit" value="Enviar mensaje">
-            <nav>
-                <a href="respuesta_mensaje.php">Simular que se envía el mensaje</a>
-            </nav>
-            
+            <input class="boton" type="submit" value="Enviar mensaje">            
         </form>
 
 <?php

7e59cef77cf752d66c26a9117a5e0fcfc7f1bf77
rcs64 <rcs64@gcloud.ua.es>
2025-11-16 21:09:50 +0100

    Corrección db e index casi terminado

--- includes/cerrarDB.php
+++ includes/cerrarDB.php
@@ -1,6 +1,4 @@
 <?php
-
 $resultado->free();
 $db->close();
-
 ?>
--- index.php
+++ index.php
@@ -5,23 +5,18 @@
     $acceder = "Acceder";
     $css = "css/index.css";
     include 'includes/header.php';
-    include 'includes/iniciarDB.php';
 
 
-    $resultado = $db->query('SELECT * FROM anuncios ORDER BY FRegistro ASC');
-    if (!$resultado) {
-        die('Error:  ' . $db->error);
+    $resultado = $db->query('SELECT FPrincipal, Alternativo, Titulo, Precio, FRegistro, Pais, Ciudad, Texto, NomTAnuncio FROM anuncios a, TiposAnuncios ta WHERE a.TAnuncio = ta.IdTAnuncio ORDER BY FRegistro DESC'); // query a la db donde se filtra ya por FRegistro para obtener los mas recientes
+    if (!$resultado) { // comprobacion de si hay resultado
+        die('Error:  ' . $db->error); // para y da el error
     }
 
     $anuncios = [];
 
-    while ($fila = $resultado->fetch_array(MYSQLI_ASSOC)) {
+    while ($fila = $resultado->fetch_array(MYSQLI_ASSOC)) { // hago fetch con array asociativo y guardo las filas en $anuncios por orden de la query
         $anuncios[] = $fila;
     }
-
-    echo $anuncios[0]['Precio'];
-
-
 
 ?>
         <section id="sectio_barraNav">
@@ -34,71 +29,50 @@
         <h1>Inicio</h1>
         <section id="sectionArticulos">
             <ul id="ul_articulos">
-                <li>
-                    <article>
-                        <a href="anuncio.php?idAnuncio=1">
-                            <img class="imagen_articulo" src="img/<?php echo $anuncios[0]['fotos'][0][0]; ?>" alt="<?php echo $anuncios[0]['fotos'][1][0]; ?>">
-                        </a>
-                        <a href="anuncio.php?idAnuncio=1" class="a_tituloPublicacion">
-                            <h2><?php echo $anuncios[0]['titulo']; ?></h2>
-                        </a>  
-                        <p class="fecha">Fecha publicación: <time datetime="2025-11-09">09-11-2025</time></p>
-                        <p class="precio">Precio: <?php echo $anuncios[0]['precio']?>€/mes</p>
-                        <p class="pais">País: <?php echo $anuncios[0]['pais']; ?></p>
-                        <p class="ciudad">Ciudad: <?php echo $anuncios[0]['ciudad']; ?></p>
-                        <p class="p_descripcionA"><?php echo substr($anuncios[0]['texto'], 0, 100) . '...'; ?></p>    
-                    </article>       
-                </li>
-                <li>
-                    <article>
-                        <a href="alerta.php">
-                            <img class="imagen_articulo" src="img/<?php echo $anuncios[1]['fotos'][0][0]; ?>" alt="<?php echo $anuncios[1]['fotos'][1][0]; ?>">
-                        </a>
-                        <a href="alerta.php" class="a_tituloPublicacion">
-                            <h2><?php echo $anuncios[1]['titulo']; ?></h2>
-                        </a>  
-                        <p class="fecha">Fecha publicación: <time datetime="2025-11-08">08-11-2025</time></p>
-                        <p class="precio">Precio: <?php echo $anuncios[1]['precio']?>€</p>
-                        <p class="pais">País: <?php echo $anuncios[1]['pais']; ?></p>
-                        <p class="ciudad">Ciudad: <?php echo $anuncios[1]['ciudad']; ?></p>
-                        <p class="p_descripcionA"><?php echo substr($anuncios[1]['texto'], 0, 100) . '...'; ?></p>    
-                    </article> 
-                </li>
-                <li>
-                    <article>
-                        <a href="anuncio.php?idAnuncio=3">
-                            <img class="imagen_articulo" src="img/<?php echo $anuncios[0]['fotos'][0][0]; ?>" alt="<?php echo $anuncios[0]['fotos'][1][0]; ?>">
-                        </a>
-                        <a href="anuncio.php?idAnuncio=3" class="a_tituloPublicacion">
-                            <h2><?php echo $anuncios[0]['titulo']; ?></h2>
-                        </a>  
-                        <p class="fecha">Fecha publicación: <time datetime="2025-11-09">09-11-2025</time></p>
-                        <p class="precio">Precio: <?php echo $anuncios[0]['precio']?>€/mes</p>
-                        <p class="pais">País: <?php echo $anuncios[0]['pais']; ?></p>
-                        <p class="ciudad">Ciudad: <?php echo $anuncios[0]['ciudad']; ?></p>
-                        <p class="p_descripcionA"><?php echo substr($anuncios[0]['texto'], 0, 100) . '...'; ?></p>    
-                    </article>
-                </li>
-                <li>
-                    <article>
-                        <a href="anuncio.php?idAnuncio=4">
-                            <img class="imagen_articulo" src="img/<?php echo $anuncios[1]['fotos'][0][0]; ?>" alt="<?php echo $anuncios[1]['fotos'][1][0]; ?>">
-                        </a>
-                        <a href="anuncio.php?idAnuncio=4" class="a_tituloPublicacion">
-                            <h2><?php echo $anuncios[1]['titulo']; ?></h2>
-                        </a>  
-                        <p class="fecha">Fecha publicación: <time datetime="2025-11-08">08-11-2025</time></p>
-                        <p class="precio">Precio: <?php echo $anuncios[1]['precio']?>€</p>
-                        <p class="pais">País: <?php echo $anuncios[1]['pais']; ?></p>
-                        <p class="ciudad">Ciudad: <?php echo $anuncios[1]['ciudad']; ?></p>
-                        <p class="p_descripcionA"><?php echo substr($anuncios[1]['texto'], 0, 100) . '...'; ?></p>    
-                    </article>
-                </li>
-            </ul>
+                <?php
+
+                for($i=0; $i<5; $i++) {    
+                    echo '<li>
+                            <article>
+                                <a href="anuncio.php?idAnuncio=1">
+                                    <img class="imagen_articulo" src="img/'.htmlspecialchars($anuncios[$i]['FPrincipal']).'" alt="'.htmlspecialchars($anuncios[$i]['Alternativo']).'">
+                                </a>
+                                <a href="anuncio.php?idAnuncio=1" class="a_tituloPublicacion">
+                                    <h2>'.htmlspecialchars($anuncios[$i]['Titulo']).'</h2>
+                                </a>';  
+
+                                
+
+                                $raw = $anuncios[$i]['FRegistro'];              // ej. "2025-11-09 14:30:00"
+                                if (!empty($raw)) {
+                                    $dt = new DateTime($raw);
+                                    $iso = $dt->format('c');                   // formato ISO 8601 con offset: 2025-11-09T14:30:00+01:00
+                                    $visible = $dt->format('d-m-Y');           // formato de visualización: 09-11-2025
+                                } else {
+                                    $iso = '';
+                                    $visible = 'Fecha no disponible';
+                                }
+
+                                $tipo_precio = '';
+                                
+                                if($anuncios[$i]['NomTAnuncio'] === 'Venta')
+                                    $tipo_precio = '€';
+                                else
+                                    $tipo_precio = '€/mes';
+                            
+
+                                echo '<p class="fecha">Fecha publicación: <time datetime="'.htmlspecialchars($iso).'">'.htmlspecialchars($visible).'</time></p>
+                                <p class="precio">Precio:'.htmlspecialchars($anuncios[$i]['Precio']).htmlspecialchars($tipo_precio).'</p>
+                                <p class="pais">País:'.htmlspecialchars($anuncios[$i]['Pais']).'</p>
+                                <p class="ciudad">Ciudad:'.htmlspecialchars($anuncios[$i]['Ciudad']).'</p>
+                                <p class="p_descripcionA">'.htmlspecialchars(substr($anuncios[$i]['Texto'], 0, 100) . '...').'</p>    
+                            </article>       
+                        </li>';
+                    }
+                ?>
         </section>
 
 <?php
-    include 'includes/cerrarDB.php';
     include 'includes/footer.php';
 ?>
 

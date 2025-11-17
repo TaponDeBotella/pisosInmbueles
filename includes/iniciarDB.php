8f4c66cbdfff7b0abbf549721e32f0b051c5ba30
rcs64 <rcs64@gcloud.ua.es>
2025-11-17 02:00:04 +0100

    he vuelto a quitar el iniciarDB

--- includes/iniciarDB.php
+++ includes/iniciarDB.php
@@ -1,9 +1,8 @@
 <?php
-$db = mysqli_connect('localhost:3306', 'root', '');
-$db->select_db('pisosbd');
+$db = mysqli_connect('localhost', 'u360956521_admin', 'x3uPYWPW:');
+$db->select_db('u360956521_pisosBD');
 ?>
 
 
-<!-- $db = mysqli_connect('localhost', 'u360956521_admin', 'x3uPYWPW:');
-$db->select_db('u360956521_pisosBD'); -->
-
+<!-- $db = mysqli_connect('localhost:3306', 'root', '');
+$db->select_db('pisosbd'); -->
